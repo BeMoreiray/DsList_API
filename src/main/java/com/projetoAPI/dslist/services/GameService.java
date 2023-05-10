d7d1ddb24c8ca623919b2e8433c0e915fb1f2d2a
Beatriz Moreira Silva <99758926+BeMoreiray@users.noreply.github.com>
2023-05-09 21:31:04 -0300

    get game by id

--- src/main/java/com/projetoAPI/dslist/services/GameService.java
+++ src/main/java/com/projetoAPI/dslist/services/GameService.java
@@ -4,16 +4,27 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.projetoAPI.dslist.dto.GameDTO;
 import com.projetoAPI.dslist.dto.GameMinDTO;
 import com.projetoAPI.dslist.entities.Game;
 import com.projetoAPI.dslist.repositories.GameRepository;
+
+
 
 @Component //Annotation que permite que essa classe torne parte do sistema spring como outras classes da biblioteca, podemos usar a annotation @Service
 public class GameService {
 	@Autowired //componentes da biblioteca se complementão para formar o sistema, aqui estou conectando o repository com o service usando essa annotation
 	private GameRepository gamerepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gamerepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gamerepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
